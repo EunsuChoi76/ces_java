21c69edd6bddb784ef836a7d757408480bba8d2e
EunsuChoi76 <dmstnchl@gmail.com>
2022-07-31 23:33:04 +0900

    20220731 한글

--- sw_pro/WormHole_1865.java
+++ sw_pro/WormHole_1865.java
@@ -2,15 +2,20 @@ package ces_java.sw_pro;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class WormHole_1865 {
+	
+	static int N, M, W;
 
 	public static void main(String[] args) throws IOException  {
 		// TODO Auto-generated method stub
+		
+		System.setIn(new FileInputStream("D:\\git\\javaWorkspace\\ces\\src\\ces_java\\sw_pro\\input\\WormHole_1865.txt"));
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,9 +24,16 @@ public class WormHole_1865 {
 		
 		int TC = Integer.parseInt(st.nextToken());
 		
-		//
+		
+		
+		
 		for(int i=0; i<TC; i++) {
 			st = new StringTokenizer(br.readLine());
+			
+			// N: 정점 M:간선 W:count of wormhole
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
 		}
 	}
 
