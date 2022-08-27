903e4db1948543cc513738c4b0784abd60d927dd
EunsuChoi76 <dmstnchl@gmail.com>
2022-08-27 12:43:11 +0900

    Create SW_PRO_Pretest_20220827_3.java

---
+++ sw_pro/SW_PRO_Pretest_20220827_3.java
@@ -0,0 +1,11 @@
+package ces_java.sw_pro;
+
+public class SW_PRO_Pretest_20220827_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("3333333");
+
+	}
+
+}
