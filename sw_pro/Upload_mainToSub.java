4abc7e5f19bcb6a05911147c4e7eb7da18e64bc3
EunsuChoi76 <dmstnchl@gmail.com>
2022-08-27 13:03:08 +0900

    Create Upload_mainToSub.java

---
+++ sw_pro/Upload_mainToSub.java
@@ -0,0 +1,10 @@
+package ces_java.sw_pro;
+
+public class Upload_mainToSub {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Upload_mainToSub");
+
+	}
+
+}
